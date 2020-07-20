2b48f0b9483da02d719eec658008fe21a76fa9fc
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-20 14:52:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,14 @@
 def new_hash
   # return an empty hash
+  {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  {
+    name: "Nunny"
+  }
+  returns my_hash[name]
 end
 
 def pioneer
